bc0f5a2a4f9984f2a0bb37490b155b33ce742dc7
viator3m <viator3m@gmail.com>
2022-07-08 17:10:56 +0300

    Problem H solved

--- sprint_1_introduction/H_sum_binary.py
+++ sprint_1_introduction/H_sum_binary.py
@@ -1,18 +1,34 @@
+from itertools import zip_longest
+from typing import Tuple
+
+
 def sum_binary(x: str, y: str) -> str:
-    x = list(map(int, x[::-1]))
-    y = list(map(int, y[::-1]))
-    for array in (x, y):
-        for i in range(len(array)):
-            array[i] = array[i] * (2 ** i)
-    summ = sum(x) + sum(y)
+    summ = ''
+    carry_one = 0
 
-    binary = ''
-    while summ >= 1:
-        res = summ // 2
-        binary += str(summ % 2)
-        summ = res
+    for arr in zip_longest(x[::-1], y[::-1], fillvalue='0'):
+        temp_sum = int(arr[0]) + int(arr[1]) + carry_one
+        if temp_sum == 0:
+            summ += '0'
+        elif temp_sum == 1:
+            summ += '1'
+            carry_one = 0
+        elif temp_sum == 2:
+            summ += '0'
+            carry_one = 1
+        elif temp_sum == 3:
+            summ += '1'
+            carry_one = 1
+    if carry_one:
+        summ += '1'
+    return summ[::-1]
 
-    return binary[::-1]
+
+def read_input() -> Tuple[str, str]:
+    first_number = input().strip()
+    second_number = input().strip()
+    return first_number, second_number
 
 
-print(sum_binary(str(input()), str(input())))
+first_number, second_number = read_input()
+print(sum_binary(first_number, second_number))
