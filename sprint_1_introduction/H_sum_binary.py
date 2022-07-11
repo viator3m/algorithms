579b13c5a088d209aaa95a854f31b782c2b461ec
viator3m <viator3m@gmail.com>
2022-07-11 12:38:13 +0300

    Problem H modify

--- sprint_1_introduction/H_sum_binary.py
+++ sprint_1_introduction/H_sum_binary.py
@@ -8,17 +8,8 @@ def sum_binary(x: str, y: str) -> str:
 
     for arr in zip_longest(x[::-1], y[::-1], fillvalue='0'):
         temp_sum = int(arr[0]) + int(arr[1]) + carry_one
-        if temp_sum == 0:
-            summ += '0'
-        elif temp_sum == 1:
-            summ += '1'
-            carry_one = 0
-        elif temp_sum == 2:
-            summ += '0'
-            carry_one = 1
-        elif temp_sum == 3:
-            summ += '1'
-            carry_one = 1
+        summ += str(temp_sum % 2)
+        carry_one = temp_sum // 2
     if carry_one:
         summ += '1'
     return summ[::-1]
