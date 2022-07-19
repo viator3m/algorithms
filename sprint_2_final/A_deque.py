5110588a68801754c2cd6a75cdf922c4babb5879
viator3m <viator3m@gmail.com>
2022-07-19 22:03:09 +0300

    Problem A_final refactor

--- sprint_2_final/A_deque.py
+++ sprint_2_final/A_deque.py
@@ -1,32 +1,42 @@
-class Queue:
-    def __init__(self, max_length):
-        self.queue = [None] * max_length
-        self.max_length = max_length
-        self.head = 0
-        self.tail = 0
-        self.size = 0
+# id 69434809
 
-    def push_front(self, item):
+from typing import List, Optional
+
+
+class Deque:
+    """Класс двухсторонней очереди."""
+    def __init__(self, max_length: int) -> None:
+        self.queue: List[Optional[int]] = [None] * max_length
+        self.max_length: int = max_length
+        self.head: int = 0
+        self.tail: int = 0
+        self.size: int = 0
+
+    def push_front(self, value: int) -> None:
+        """Добавляет элемент в начало очереди."""
         if self.size < self.max_length:
             if self.is_empty():
                 self.tail += 1
-            self.queue[self.head] = item
+            self.queue[self.head] = value
             self.size += 1
             self.head = (self.head - 1) % self.max_length
         else:
             print('error')
 
-    def push_back(self, item):
+    def push_back(self, value: int) -> None:
+        """Добавляет элемент в конец очереди."""
         if self.size < self.max_length:
             if self.is_empty():
                 self.head = self.max_length - 1
-            self.queue[self.tail] = item
+            self.queue[self.tail] = value
             self.size += 1
             self.tail = (self.tail + 1) % self.max_length
         else:
             print('error')
 
-    def pop_front(self):
+    def pop_front(self) -> int or str:
+        """Удаляет элемент из начала очереди и возвращает его.
+        Печатает 'error', если очередь пуста."""
         if self.is_empty():
             return 'error'
         self.head = (self.head + 1) % self.max_length
@@ -37,7 +47,9 @@ class Queue:
             self.head = self.tail = 0
         return result
 
-    def pop_back(self):
+    def pop_back(self) -> int or str:
+        """Удаляет элемент из конца очереди и возвращает его.
+        Печатает 'error', если очередь пуста."""
         if self.is_empty():
             return 'error'
         self.tail = (self.tail - 1) % self.max_length
@@ -48,7 +60,9 @@ class Queue:
             self.head = self.tail = 0
         return result
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
+        """Возвращает булево значение.
+        True - если очередь пуста. Иначе False."""
         return self.size == 0
 
     def __str__(self):
@@ -56,19 +70,23 @@ class Queue:
 
 
 def main():
-    commands = int(input())
+    """Основная логика программы."""
+    comm = int(input())
     max_length = int(input())
-    queue = Queue(max_length)
-    for command in range(commands):
-        command = input().split()
-        if command[0] == 'push_front':
-            queue.push_front(command[1])
-        elif command[0] == 'push_back':
-            queue.push_back(command[1])
-        elif command[0] == 'pop_front':
-            print(queue.pop_front())
-        elif command[0] == 'pop_back':
-            print(queue.pop_back())
+    deque = Deque(max_length)
+    COMMANDS = {
+        'push_front': deque.push_front,
+        'push_back': deque.push_back,
+        'pop_front': deque.pop_front,
+        'pop_back': deque.pop_back,
+    }
+
+    for command in range(comm):
+        command, *value = input().split()
+        if value:
+            COMMANDS[command](int(*value))
+        else:
+            print(COMMANDS[command]())
 
 
 if __name__ == '__main__':
