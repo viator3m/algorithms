28846cb3da046b4277f26afca5afb72326f8abd7
viator3m <viator3m@gmail.com>
2022-08-15 13:52:28 +0300

    Problem P solved

---
+++ sprint_3/P_partial_sort.py
@@ -0,0 +1,33 @@
+def get_block(array):
+    block = values = indexes = 0
+    for i in range(len(array)):
+        values += array[i]
+        indexes += i
+        if values == indexes:
+            block += 1
+            values = indexes = 0
+        i += 1
+    return block
+
+
+class Test:
+    def test_1(self):
+        a = [3, 2, 0, 1, 4, 6, 5]
+        assert get_block(a) == 3
+
+    def test_2(self):
+        a = [0, 1, 3, 2]
+        assert get_block(a) == 3
+
+    def test_3(self):
+        a = [3, 6, 7, 4, 1, 5, 0, 2]
+        assert get_block(a) == 1
+
+
+def main():
+    _, array = int(input()), [int(i) for i in input().split()]
+    print(get_block(array))
+
+
+if __name__ == '__main__':
+    main()
